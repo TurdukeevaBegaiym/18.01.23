6f43fbfe90cc13477befc99e0ad608c952aa3cd3
begaiyma <turdukeeva064@gmail.com>
2023-01-18 15:10:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+let myName = "Begaiym";
+
+console.log(myName);
+console.log("My name's length is" + myName.length); 
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase());
+
+
+let pi = 3.141593;
+
+console.log(pi);
+console.log(pi.toFixed(2));
+console.log(pi.toFixed(0));
+
+let example1 = 2 + 2;
+let example2 = 2 - 2;
+let example3 = 2 * 2;
+let example4 = 2 / 2;
+let example5 = 2 % 2;
+let example6 = 2 ** 2;//степень квадрат
+let example7 = 81 **(1/2)//квадратный корень
+let example8 = 5 % 2;//остаток
+
+let example9 = "Hello" + "World" + '!';
+
+let example10 = 4 + 10 + 12;
+let example11 = 1 + 5 *3;
+
+let example12 = 5 + `3`;
+let example13 = `5` * 3;//15
+let example14 = `a` * 3;//Nan
+
+let example15 = -15;
+let example16 = +`15`;
+console.log(example9);
+console.log (example7);
+console.log(example12);
+console.log(example13);
+console.log(example14);
+console.log(example16);
+

50d3d8f82a677e31e180916937415da6a9394a48
begaiyma <turdukeeva064@gmail.com>
2023-01-18 16:01:55 +0600

    exercises

---
+++ excercises.js
@@ -0,0 +1,37 @@
+let height = 5;
+let width = 8;
+let triangleArea = height * width /2 ;
+console.log (triangleArea);
+
+let hours = 56;
+let minutes = 56 * 60;
+console.log (minutes + " минут ");
+
+let fullWorkday = 8;
+let days = 8 
+let money = 8 * 8 * 50;
+console.log(money + " долллар ");
+
+let klient = 1566;
+let som = 1566 / 100 * 15;
+console.log (som);
+
+let twoPoint = 8 * 2;
+let threPoint = 6 * 3;
+let point = twoPoint + threPoint;
+console.log (point);
+
+let topliva = 22;
+let distance = 100;
+let tractor = 100 / 22 * 100;
+console.log (tractor);
+
+let cow = 5;
+let chiсken = 6;
+let sheep = 12;
+let amount = (5 + 12) * 4 + 6 * 2;
+console.log (amount);
+
+
+
+
